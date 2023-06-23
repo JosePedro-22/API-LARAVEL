3b906fcbb72b3c3da1a96b80ece5fdf70772cec9
Jose Pedro <jose.mesquita@seati.ma.gov.br>
2023-06-23 17:41:26 -0300

    started api laravel

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -10,13 +10,6 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * The path to your application's "home" route.
-     *
-     * Typically, users are redirected here after authentication.
-     *
-     * @var string
-     */
     public const HOME = '/home';
 
     /**
@@ -30,11 +23,11 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
-                ->prefix('api')
+//                ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+//            Route::middleware('web')
+//                ->group(base_path('routes/web.php'));
         });
     }
 }
